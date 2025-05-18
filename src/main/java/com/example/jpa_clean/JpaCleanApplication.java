11e03e5393812f1779b4f57928013b3c65107586
Gabriel Carneiro <gabriel.brutu@gmail.com>
2025-05-18 13:32:28 -0300

    chore: structure and libs

---
+++ src/main/java/com/example/jpa_clean/JpaCleanApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpa_clean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaCleanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaCleanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jpa_clean/JpaCleanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpa_clean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaCleanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
